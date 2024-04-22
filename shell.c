04da1674a9dc1204be40044a6f273b5a7339a9c4
TonyHolby <tonynemoutheholberton@gmail.com>
2024-04-22 18:42:47 +0000

    Fix code and handle errors in shell.c

--- shell.c
+++ shell.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
 #include <sys/wait.h>
+#include <signal.h>
 
 #define MAX_COMMAND_LENGTH 1024
+
+void sigint_handler(int signum)
+{
+	printf("\nSIGINT signal : %d.\n", signum);
+	exit(EXIT_FAILURE);
+}
 
 /**
  * main - Displays a prompt and waits for the user to type a command.
@@ -14,29 +23,34 @@
 
 int main(void)
 {
-	char *command = NULL;
-	size_t command_len = MAX_COMMAND_LENGTH;
+	char *line = NULL;
+	size_t length = 0;
 	ssize_t read;
 	char *args[] = {NULL, NULL};
 	pid_t pid;
+	int status;
 
-	command = (char *)malloc(command_len * sizeof(char));
-	if (command == NULL)
+	if (signal(SIGINT, sigint_handler) == SIG_ERR)
 	{
-		perror("Memory allocation error");
+		perror("Handler errors failed");
 		exit(EXIT_FAILURE);
 	}
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
 			printf("#simple_shell$ ");
-		read = getline(&command, &command_len, stdin);
+		read = getline(&line, &length, stdin);
 
 		if (read == -1)
 		{
+			printf("\n");
 			break;
 		}
-		command[read - 1] = '\0';
+		if (read == 1)
+		{
+			continue;
+		}
+		line[read - 1] = '\0';
 		pid = fork();
 
 		if (pid < 0)
@@ -46,18 +60,21 @@ int main(void)
 		}
 		else if (pid == 0)
 		{
-			args[0] = command;
+			args[0] = line;
 			args[1] = NULL;
-
-			if (command == "\n")
-				exit(EXIT_FAILURE);
-			execve(command, args, NULL);
+			execve(line, args, NULL);
 			perror("Execve failed");
 			exit(EXIT_FAILURE);
 		}
 		else
-			wait(NULL);
+		{
+			if (wait(&status) == -1)
+			{
+				perror("Wait failed");
+				exit(EXIT_FAILURE);
+			}
+		}
 	}
-	free(command);
+	free(line);
 	return (0);
 }
