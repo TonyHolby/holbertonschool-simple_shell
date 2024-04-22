d7feed920a3feefa9b9c0a7c7acbf6321cc16c75
RosarioGle <8824@holbertonstudents.com>
2024-04-22 13:41:53 +0000

    fixed compilation error in shell.c file

--- shell.c
+++ shell.c
@@ -5,32 +5,39 @@
 
 #define MAX_COMMAND_LENGTH 1024
 
+/**
+ * main - Displays a prompt and waits for the user to type a command.
+ * then executes the command typed by the user.
+ *
+ * Return: Always 0
+ */
+
 int main(void)
 {
-	char *command;
+	char *command = NULL;
 	size_t command_len = MAX_COMMAND_LENGTH;
+	ssize_t read;
+	char *args[] = {NULL, NULL};
+	pid_t pid;
 
 	command = (char *)malloc(command_len * sizeof(char));
-    
 	if (command == NULL)
 	{
 		perror("Memory allocation error");
 		exit(EXIT_FAILURE);
 	}
-
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
 			printf("#simple_shell$ ");
-
-		ssize_t read = getline(&command, &command_len, stdin);
+		read = getline(&command, &command_len, stdin);
 
 		if (read == -1)
 		{
 			break;
 		}
 		command[read - 1] = '\0';
-		pid_t pid = fork();
+		pid = fork();
 
 		if (pid < 0)
 		{
@@ -39,17 +46,15 @@ int main(void)
 		}
 		else if (pid == 0)
 		{
-			char *args[] = {command, NULL};
+			args[0] = command;
+			args[1] = NULL;
 			execve(command, args, NULL);
 			perror("Execve failed");
 			exit(EXIT_FAILURE);
 		}
 		else
-		{
 			wait(NULL);
-		}
 	}
-
 	free(command);
-	return 0;
+	return (0);
 }
